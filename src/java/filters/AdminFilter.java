89cf0582369249e64bcc4e057b29290693a1c58f
wenchi <wenchi@DESKTOP-QFUKDCQ>
2021-11-12 08:35:24 -0800

    Feat: Add Admin Filter for security function

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,51 @@
+
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+
+public class AdminFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+       //any code before chain.doFIlter will execute before the servlet
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        
+         if (user.getRole().getRoleId() != 1){
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("notes");
+            return; //very important
+        }
+        // This will either call upon the next filter in the chain,
+        // or it will load the requested servlet;
+        chain.doFilter(request, response);
+        
+        // Any code after chain.doFIlter will be execute after the servlet
+    
+    }
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+       }
+
+    @Override
+    public void destroy() {
+        }
+    
+   
+    
+}
